e717115d6885782902c67b6560a5a24c3e7ec750
Soumyajit-io <soumyajit010805@gmail.com>
2025-04-13 11:37:46 +0530

    code added

--- 06. Arrays/27mergeTwoSortedArray.cpp
+++ 06. Arrays/27mergeTwoSortedArray.cpp
@@ -1,67 +1,59 @@
-//Merge two sorted Arrays
 #include<iostream>
 #include<vector>
-#include <algorithm>
 using namespace std;
 
-void merge(vector<int>& v1,vector<int>& v2,vector<int>& v3){
-    int x=0,y=0,z=0;
-    for(int i = 0; i<v1.size()+v2.size();i++){
-        
-        if(v1[x]<v2[y]){
-            v3.push_back(v1[x]);
-            x++;
-        }
-        else if(v1[x]>v2[y]){
-            v3.push_back(v2[y]);
-            y++;
+void merge(const vector<int>& v1, const vector<int>& v2, vector<int>& v3) {
+    int i = 0; // v1
+    int j = 0; // v2
+
+    while (i < v1.size() && j < v2.size()) {
+        if (v1[i] < v2[j]) {
+            v3.push_back(v1[i]);
+            i++;
+        } else {
+            v3.push_back(v2[j]);
+            j++;
         }
     }
-    for(int i=0;i<v1.size()+v2.size();i++){
-        cout<<v3[i]<<" ";
 
+    // For remaining elements in v1
+    while (i < v1.size()) {
+        v3.push_back(v1[i]);
+        i++;
     }
 
+    // For remaining elements in v2
+    while (j < v2.size()) {
+        v3.push_back(v2[j]);
+        j++;
+    }
+
+    // Output the merged array
+    for (int i = 0; i < v3.size(); i++) {
+        cout << v3[i] << " ";
+    }
+    cout << endl;
 }
-int main (){
-    vector<int> v1;
-    v1.push_back(2);
-    v1.push_back(8);
-    v1.push_back(28);
-    v1.push_back(30);
-    v1.push_back(48);
-    v1.push_back(70);
-    v1.push_back(77);
-    v1.push_back(88);
-    v1.push_back(91);
-    v1.push_back(100);
 
-
-    vector<int> v2;
-    v2.push_back(5);
-    v2.push_back(20);
-    v2.push_back(43);
-    v2.push_back(50);
-    v2.push_back(60);
-    v2.push_back(89);
-    v2.push_back(90);
-    
-
+int main() {
+    vector<int> v1 = {2, 8, 28, 30, 48, 70, 77, 88, 91, 100};
+    vector<int> v2 = {5, 20, 43, 50, 60, 89, 90};
     vector<int> v3;
 
-
-  
-    for(int i=0;i<v1.size();i++){
-        cout<<v1[i]<<" ";
-
+    cout << "v1: ";
+    for (int i = 0; i < v1.size(); i++) {
+        cout << v1[i] << " ";
     }
-    cout<<endl;
-    for(int i=0;i<v2.size();i++){
-        cout<<v2[i]<<" ";
+    cout << endl;
 
+    cout << "v2: ";
+    for (int i = 0; i < v2.size(); i++) {
+        cout << v2[i] << " ";
     }
-    cout<<endl;
-    
-    merge(v1,v2,v3);
+    cout << endl;
 
+    cout << "Merged: ";
+    merge(v1, v2, v3);
+
+    return 0;
 }
