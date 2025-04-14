da360348763e870656ffb7db3fed55bd59f046b6
Soumyajit-io <soumyajit010805@gmail.com>
2025-04-14 09:06:35 +0530

    code added

--- 07. Strings/01basic.cpp
+++ 07. Strings/01basic.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){// Strings are basically char Array
-    // char str[3]={'a','b','c'};
-    char str[3]= "abc";
+     char str[3]={'a','b','c'};      //'\0':it is a null charecter
+    // char str[3]= "ab";
     for(int i=0;i<3;i++){
         cout<<str[i]<<" ";
     }
+    for(int i=0;str[i]!='\0';i++){
+        cout<<str[i]<<" ";
+    }
+    cout<<str;
 }
---
+++ 07. Strings/02DeclarationOfString.cpp
@@ -0,0 +1,11 @@
+//String as a data type
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main(){
+     string str= "soumo";
+     cout<<str<<endl;
+     cout<<str[0];
+
+}
