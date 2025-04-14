e09f935fe9e04c329bc121b97c7978e9c0314ba0
Soumyajit-io <soumyajit010805@gmail.com>
2025-04-14 08:49:03 +0530

    New Data structure added

---
+++ 07. Strings/01basic.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){// Strings are basically char Array
+    // char str[3]={'a','b','c'};
+    char str[3]= "abc";
+    for(int i=0;i<3;i++){
+        cout<<str[i]<<" ";
+    }
+}
