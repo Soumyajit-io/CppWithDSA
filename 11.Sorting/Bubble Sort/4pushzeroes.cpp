549dbbc9ab227f53a3b1c2549866d9f981995f94
Soumyajit-io <soumyajit010805@gmail.com>
2025-06-01 17:16:15 +0530

    code added

--- 11.Sorting/Bubble Sort/4pushzeroes.cpp
+++ 11.Sorting/Bubble Sort/4pushzeroes.cpp
@@ -1,4 +1,4 @@
-//  push zeroes
+//  Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
     void moveZeroes(vector<int>& nums) {
         for(int i=0;i<nums.size()-1;i++){
