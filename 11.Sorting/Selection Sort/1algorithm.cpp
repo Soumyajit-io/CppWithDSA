6876fc5c89ea5156d55b8ed7cd4a951f5e1e09ac
Soumyajit-io <soumyajit010805@gmail.com>
2025-06-02 11:32:19 +0530

    code added

---
+++ 11.Sorting/Selection Sort/1algorithm.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<climits>
+
+using namespace std;
+int main(){
+    int arr[5]={5,4,3,2,1};
+    
+    
+    for(int i=0;i<5-1;i++){//
+        int min=INT_MAX;
+        int mindx =-1;
+        for(int  j=i;j<5;j++){
+            if(arr[j]<min){
+                min =arr[j];
+                mindx =j;
+            }
+        }
+        swap(arr[i],arr[mindx]);
+    }
+
+
+
+    for(int ele : arr){//for each element
+        cout<<ele<<" ";
+    }
+}
