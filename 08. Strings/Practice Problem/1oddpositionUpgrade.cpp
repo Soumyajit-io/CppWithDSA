da0de463038b064bc9b89fb2017fa18772996c92
Soumyajit-io <soumyajit010805@gmail.com>
2025-06-04 18:42:00 +0530

    code added

---
+++ 08. Strings/Practice Problem/1oddpositionUpgrade.cpp
@@ -0,0 +1,14 @@
+//make the odd position #
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    for(int i=0;str[i]!='\0';i++){
+        if(i%2!=0){
+            str[i]='#';
+        }
+    }
+    cout<<str;
+}
